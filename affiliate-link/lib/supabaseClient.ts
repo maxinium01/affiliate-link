aa8a8b4fbc1c84ad8bd94909919aea46fbfb9346
maxinium01 <chayasatechayjaroen@gmail.com>
2025-10-08 10:22:01 +0700

    Update supabaseClient.ts

--- affiliate-link/lib/supabaseClient.ts
+++ affiliate-link/lib/supabaseClient.ts
@@ -1,1 +1,8 @@
+import { createClient } from '@supabase/supabase-js';
+
+export const supabaseBrowser = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON!,
+  { auth: { persistSession: false } }
+);
 
